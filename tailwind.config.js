b43442d8883e83f174d06cfed90ed9d740094dd9
Isaleo guimaraes <isaleoguimaraes284@gmail.com>
2025-05-17 15:34:33 -0300

    Fronend finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home": "url('../img/banner.png')",
+      },
+    },
+  },
+  plugins: [],
+};
